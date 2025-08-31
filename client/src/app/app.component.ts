b0a1ff671ebc5a2b036f72152c77ed98cc9c4513
run1206 <run1206@gmail.com>
2025-08-31 01:41:13 -0400

    End of section 5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { User } from './_models/user';
 import { AccountService } from './_services/account.service';
@@ -11,23 +10,14 @@ import { AccountService } from './_services/account.service';
 export class AppComponent implements OnInit{
   title = 'The Sample App';
   users: any;
-  constructor(private http: HttpClient, private accountService: AccountService){}
+  constructor(private accountService: AccountService){}
   
   ngOnInit() {
-    this.getUsers();
     this.setCurrentUser();
   }
 
   setCurrentUser() {
     const user: User = JSON.parse(localStorage.getItem('user'));
     this.accountService.setCurrentUser(user);
-  }
-
-  getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe(resp => {
-      this.users = resp;
-    }, error => {
-      console.log(error);
-    });
   }
 }
